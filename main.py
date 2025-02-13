391800c8478ebe699ad058f9e1f95413d79b75b8
terenceithaque <96685979+terenceithaque@users.noreply.github.com>
2025-02-13 15:21:46 +0100

    Ajouté la fonction jeu()

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 "main.py contient la logique du jeu"
 # Importer les modules nécessaires au jeu
-import modules.nombre as nb
-import modules.grille as grl
-import modules.mouvement as mvm
+import modules.nombre as nombre
+import modules.grille as grille
+import modules.mouvement as mov
+
+
+
+def jeu():
+    "Boucle principale du jeu"
+    # Initialiser la variable d'exécution
+    execution = True
+    while execution:
+        
 
 
