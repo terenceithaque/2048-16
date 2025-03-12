e00cfce97a5423a27f61571978462fc86dcad837
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-03-12 19:58:27 +0100

    Mis à jour description de la classe Score

--- score.py
+++ score.py
@@ -9,8 +9,6 @@ class Score:
     def __init__(self, fenetre:pygame.Surface) -> None:
         """Initialise la classe Score.
            Les propriétés suivantes permettent de régler les attributs de la classe:
-               - valeur: nombre entier représentant la valeur de départ (0 généralement).
-               - valeur_max: Meilleur score réalisé.
                - fenetre: fenêtre pygame sur laquelle afficher le score."""
         
         # Initialisation des valeurs
