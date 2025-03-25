148b488fdb944523746fb97f38d44d15ac62fb67
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-03-25 19:54:50 +0100

    Ajouté deplacer() à la classe Tuile, vide

--- tuile.py
+++ tuile.py
@@ -1,1 +1,28 @@
 "tuile.py permet de gérer les tuiles du jeu"
+import pygame
+
+class Tuile:
+    "Représente une tuile du jeu"
+    def __init__(self, valeur:int, x:int, y:int, ecran:pygame.Surface) -> None:
+        """Initalise la tuile.
+            valeur: valeur de la tuile, un entier.
+            x: position x de la tuile.
+            y: position y de la tuile."""
+        
+
+        # Initialiser la valeur de la tuile
+        self.valeur = valeur
+
+        # Positions x et y de la tuile
+        self.x = x
+        self.y = y
+
+        # Ecran sur lequel afficher la tuile
+        self.ecran = ecran
+
+
+    def deplacer(self, direction:str) -> None:
+        "Déplace la tuile dans une direction donnée."
+        pass     
+
+    
